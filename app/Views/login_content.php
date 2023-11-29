aa987760086ac8dab87a6295864daf2b9b2a1d66
Muhammad Fadhli Adam <2010631170095@student.unsika.ac.id>
2023-11-29 19:17:59 +0700

    Update Login User In Progress

---
+++ app/Views/login_content.php
@@ -0,0 +1,7 @@
+<div class="d-grid gap-2">
+    <button type="button" class="btn btn-outline-primary" onclick=""><i class="bi bi-google"></i> Login dengan Google</button>
+</div>
+<p class="fw-bold mt-3 mb-3"><a href="https://accounts.google.com/signup" target="_blank" rel="noopener noreferrer">Tidak memiliki akun Google</a></p>
+<span class="d-inline">Dengan terhubungnya akun Google, maka anda setuju dengan
+    <a href="" class="fw-semibold">ketentuan</a> dan <a href="" class="fw-semibold">kebijakan privasi</a> kami.
+</span>
