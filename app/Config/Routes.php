7a9869e3d27a0d05eb3e6d329aebbf4fe3405ad0
Muhammad Fadhli Adam <2010631170095@student.unsika.ac.id>
2023-11-30 19:05:18 +0700

    Add entity, model, and login logic

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,11 +1,11 @@
 <?php
 
-use App\Controllers\Home;
 use CodeIgniter\Router\RouteCollection;
 
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', [Home::class,'index'])->setDefaultNamespace('home');
+$routes->get('/', 'Home::index');
+
 $routes->get('/login', 'Login::index');
 $routes->get('/login/process', 'Login::process');
--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -3,11 +3,13 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\UserModel;
 use Google_Client;
 
 class Login extends BaseController
 {
     protected $googleClient;
+    protected $user;
 
     public function __construct()
     {
@@ -31,21 +33,31 @@ class Login extends BaseController
 
     public function process()
     {
-        $token = $this->googleClient->fetchAccessTokenWithAuthCode($this->request->getVar('code'));
-        if (!isset($token['error'])) {
-            $this->googleClient->setAccessToken($token['access_token']);
-            $googleService = new \Google\Service\Oauth2($this->googleClient);
-            $data = $googleService->userinfo->get();
+        if (isset($_GET['code'])) {
+            $token = $this->googleClient->fetchAccessTokenWithAuthCode($this->request->getVar('code'));
+            if (!isset($token['error'])) {
+                $this->googleClient->setAccessToken($token['access_token']);
+                $googleService = new \Google\Service\Oauth2($this->googleClient);
+                $data = $googleService->userinfo->get();
 
-            $row = [
-                'id_google' => $data['id'],
-                'username' => $data['givenName'],
-                'email' => $data['email'],
-                'image' => $data['picture'],
-                'role_id' => 2,
-            ];
+                $entity = new \App\Entities\UserEntity();
+                $entity->id_google = $data['id'];
+                $entity->username = $data['givenName'];
+                $entity->email = $data['email'];
+                $entity->image = $data['picture'];
+                $entity->role_id = 1;
 
-            session()->set($row);
+                $model = new UserModel();
+                $user = $model->where('email', $entity->email)->find();
+                if ($user) {
+                    return redirect()->to('/dashboard');
+                } else {
+                    $model->save($entity);
+                    return redirect()->to('/dashboard');
+                }
+            }
+        } else {
+            return redirect()->to('/login');
         }
     }
 }
